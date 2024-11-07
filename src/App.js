4bc99b3310e9c4eca383422ad82e6d2b31c20760
AyeshaaRani <ayesharani6788@gmail.com>
2024-11-07 11:08:15 +0500

    update

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Navbar from './Components/Navbar';
 import LocomotiveScroll from 'locomotive-scroll';
 
 function App() {
+  const locomotiveScroll = new LocomotiveScroll()
   
   return (
     <div className="w-full min-h-screen  bg-zinc-900  text-white ">
